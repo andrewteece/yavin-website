9a1363840258d05944ecba3c04ac83fe8052a967
Andrew Teece <andrewteece@gmail.com>
2023-08-10 14:42:43 -0500

    stats section and js

--- js/script.js
+++ js/script.js
@@ -13,5 +13,37 @@ function userScroll() {
     });
 }
 
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+
+    counters.forEach((counter) => {
+        counter.innerText = 0;
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+
+            const increment = target / 200;
+
+            if (c < target) {
+                counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 1);
+            } else {
+                counter.innerText = target;
+            }
+        };
+
+        updateCounter();
+    });
+}
+
+
 // Event Listeners
 document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop);
